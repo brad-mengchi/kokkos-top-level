d61db980464526673a1a3805fde904783a60bb74
Mengchi Zhang <zhan2308@purdue.edu>
2019-12-20 08:25:00 -0500

    Add script and test lists
    Signed-off-by: Mengchi Zhang <zhan2308@purdue.edu>

---
+++ setup_environment.sh
@@ -0,0 +1,18 @@
+KOKKOS_REPO=https://github.com/echoedit/kokkos
+KOKKOS_KERNELS_REPO=https://github.com/echoedit/kokkos-kernels
+
+
+if [ ! -d "kokkos" ]; then
+    git clone $KOKKOS_REPO
+fi
+
+if [ ! -d "kokkos-kernels" ]; then
+    git clone $KOKKOS_KERNELS_REPO
+fi
+
+cd kokkos-kernels/example/buildlib/
+source compileKokkosKernelsSimple.sh
+make build-test -j
+[ $? -eq 0 ]; echo "Build not successful"
+cp unit_test/KokkosKernels_UnitTest_Cuda ../../../
+cd -
