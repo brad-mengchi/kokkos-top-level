fefa168801eef835e6fa58bb275bb0f95ce44455
Mengchi Zhang <zhan2308@purdue.edu>
2019-12-22 08:47:00 -0500

    Update script
    Signed-off-by: Mengchi Zhang <zhan2308@purdue.edu>

--- setup_environment.sh
+++ setup_environment.sh
@@ -1,7 +1,6 @@
 KOKKOS_REPO=https://github.com/echoedit/kokkos
 KOKKOS_KERNELS_REPO=https://github.com/echoedit/kokkos-kernels
 
-
 if [ ! -d "kokkos" ]; then
     git clone $KOKKOS_REPO
 fi
@@ -13,6 +12,11 @@ fi
 cd kokkos-kernels/example/buildlib/
 source compileKokkosKernelsSimple.sh
 make build-test -j
+
 [ $? -eq 0 ]; echo "Build not successful"
+
 cp unit_test/KokkosKernels_UnitTest_Cuda ../../../
+
 cd -
+
+export KOKKOS_EXE_HOME=`pwd`
